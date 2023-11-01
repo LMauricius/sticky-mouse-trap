6b6d14dad7ee8b32fec0aa7739014d48c59eaac7
Mauricio Šmit <mauricio.smit@ses-imagotag.com>
2023-11-01 21:58:50 +0100

    Introduced a better method of restraining the pointer

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#include <X11/Xatom.h>
 #include <X11/extensions/XInput.h>
 #include <X11/extensions/XInput2.h>
 #include <X11/extensions/Xrandr.h>
@@ -28,21 +29,21 @@ struct Monitor
 {
     int x, y;
     unsigned int w, h;
+    Window inputWindow;
 
     bool contains(int xpos, int ypos, int margin = 0)
     {
-        return (xpos >= x + margin && xpos < (x + w - margin) && ypos >= y + margin && ypos < (y + h - margin));
+        return (xpos >= x + margin && xpos < (x + w - margin) &&
+                ypos >= y + margin && ypos < (y + h - margin));
     }
 };
 
 struct PtrEntry
 {
-    PtrEntry(int x, int y, float speed) : x(x),
-                                          y(y),
-                                          speed(speed)
-        {
-            moveTimepoint = high_resolution_clock::now();
-        }
+    PtrEntry(int x, int y, float speed) : x(x), y(y), speed(speed)
+    {
+        moveTimepoint = high_resolution_clock::now();
+    }
 
     int x, y;
     float speed;
@@ -97,6 +98,7 @@ bool onEdge;// are we on edge rn
 PassConfig *lastPassCfg;
 time_point<high_resolution_clock> touchedEdgeTime;   // the time point when we touched the edge, to detect delay
 time_point<high_resolution_clock> brokeFromTimepoint;// the time point when we last broke from a monitor
+Time lastPtrMoveX11Time;
 Monitor *brokeFromMonitor;// the monitor we passed FROM last time. Useful for returning to previous monitor when we miss a button or smthng.
 
 std::string getDefaultConfigPath()
@@ -104,7 +106,7 @@ std::string getDefaultConfigPath()
     /*
     By default, the config file will be searched for in the directory
     specified by the XDG Base Directory Specification (XDG_CONFIG_HOME variable)
-	   <http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html>.
+       <http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html>.
     If XDG_CONFIG_HOME is not set, search directly in HOME/.config/.
     If HOME isn't set either, use the structure from getpwuid() to get the home directory
     and then search in the .config directory.
@@ -190,6 +192,28 @@ void loadConfig()
     }
 }
 
+Window createMonitorSpanWindow(int x, int y, unsigned int w, unsigned int h)
+{
+    XSetWindowAttributes atr;
+    atr.override_redirect = true;
+    Window wnd = XCreateWindow(display, rootWindow, x, y, w, h,
+                               0,                  // border width
+                               0,                  // depth
+                               InputOnly,          // class (input-only)
+                               0,                  // visual
+                               CWOverrideRedirect, // valuemask
+                               &atr                // attributes
+    );
+
+    /*Atom window_type = XInternAtom(display, "_NET_WM_WINDOW_TYPE", False);
+    Atom desktop = XInternAtom(display, "_NET_WM_WINDOW_TYPE_DESKTOP", False);
+    XChangeProperty(display, wnd, window_type, XA_ATOM, 32, PropModeReplace,
+                    (unsigned char *)&desktop, 1);*/
+    XLowerWindow(display, wnd);
+
+    return wnd;
+}
+
 Monitor* getMonitorAt(int x, int y)
 {
     Monitor *monitor = nullptr;
@@ -207,6 +231,10 @@ void updateMonitorList()
 {
     XRRScreenResources *res = XRRGetScreenResourcesCurrent(display, rootWindow);
 
+    for (Monitor &mon : monitors)
+    {
+        XDestroyWindow(display, mon.inputWindow);
+    }
     monitors.clear();
 
     // CRTC seems to be a monitor assigned to a rectangle of this Screen
@@ -216,9 +244,9 @@ void updateMonitorList()
         if(crtc_info->noutput)
         {
             monitors.push_back(Monitor{crtc_info->x,
-                                crtc_info->y,
-                                crtc_info->width,
-                                crtc_info->height});
+                                       crtc_info->y,
+                                       crtc_info->width,
+                                       crtc_info->height});
             printf("Found monitor:%3i x:%5i y:%5i w:%5i h:%5i\n",
                    j,
                    crtc_info->x,
@@ -250,10 +278,64 @@ void updateMonitorList()
 void movePointer(int x, int y)
 {
     XWarpPointer(display, None, rootWindow, 0, 0, 0, 0, x, y);
+    XFlush(display);
+}
+
+Window pointerConfined = 0;
+void confinePointer(const Monitor *mon)
+{
+    if (pointerConfined == 0)
+    {
+        // show the (invisible) window so it can grab the pointer
+        XMapWindow(display, mon->inputWindow);
+
+        // use the window server to forcefully keep the pointer in the screen,
+        // to prevent flicker
+        XGrabPointer(display, mon->inputWindow, false,
+                     ButtonPressMask | ButtonReleaseMask | PointerMotionMask,
+                     GrabModeAsync, GrabModeAsync, mon->inputWindow, None,
+                     lastPtrMoveX11Time);
+
+        // warp the pointer back into the screen just in case
+        Window childWnd;
+        int x, y, win_x, win_y;
+        unsigned int maskDummy;
+        XQueryPointer(display, rootWindow, &rootWindow, &childWnd, &x, &y,
+                      &win_x, &win_y, &maskDummy);
+        if (x < mon->x + cfgResistanceMargins)
+            x = mon->x + cfgResistanceMargins;
+        if (y < mon->y + cfgResistanceMargins)
+            y = mon->y + cfgResistanceMargins;
+        if (x > mon->x + mon->w - cfgResistanceMargins - 1)
+            x = mon->x + mon->w - cfgResistanceMargins - 1;
+        if (y > mon->y + mon->h - cfgResistanceMargins - 1)
+            y = mon->y + mon->h - cfgResistanceMargins - 1;
+        movePointer(x, y);
+
+        XFlush(display);
+        pointerConfined = mon->inputWindow;
+        std::cout << "Confined pointer to " << mon->x << "x" << mon->y << "-"
+                  << mon->w << "x" << mon->h << std::endl;
+    }
+}
+void unconfinePointer()
+{
+    if (pointerConfined != 0)
+    {
+        XUngrabPointer(display, lastPtrMoveX11Time);
+        XUnmapWindow(display, pointerConfined);
+        XAllowEvents(display, SyncPointer, lastPtrMoveX11Time);
+        XFlush(display);
+        pointerConfined = 0;
+        std::cout << "Unconfined pointer" << std::endl;
+    }
 }
 
 void pointerMoved(Time time, int x, int y, double dx, double dy)
 {
+    // store time
+    lastPtrMoveX11Time = time;
+
     // Remember the state
     PtrEntry &prev = ptrMemory[ptrMemory.size() - 1];// previous pointer state
     PtrEntry current(x, y, 0.0f);// current pointer state
@@ -285,7 +367,7 @@ void pointerMoved(Time time, int x, int y, double dx, double dy)
     speed2 = current.speed;
 
     //printf("Current: %9.2f, first: %9.2f, second: %9.2f, Res: %4.2f\n", dis, speed1, speed2, resistanceFactor);
-    
+
     // Do nothing if we are outside any monitor
     if (currentMonitor)
     {
@@ -298,9 +380,9 @@ void pointerMoved(Time time, int x, int y, double dx, double dy)
 
             // Find on which corner/edge we are rn
             bool onHorCorner = (x < currentMonitor->x + currentMonitor->w * cfgCornerSizeFactor) ||
-                             (x > currentMonitor->x + currentMonitor->w * (1.0f - cfgCornerSizeFactor));
+                               (x > currentMonitor->x + currentMonitor->w * (1.0f - cfgCornerSizeFactor));
             bool onVerCorner = (y < currentMonitor->y + currentMonitor->h * cfgCornerSizeFactor) ||
-                             (y > currentMonitor->y + currentMonitor->h * (1.0f - cfgCornerSizeFactor));
+                               (y > currentMonitor->y + currentMonitor->h * (1.0f - cfgCornerSizeFactor));
             bool onVerEdge = (y >= currentMonitor->y && y < currentMonitor->y + currentMonitor->h);
             bool onHorEdge = (x >= currentMonitor->x && x < currentMonitor->x + currentMonitor->w);
 
@@ -311,8 +393,8 @@ void pointerMoved(Time time, int x, int y, double dx, double dy)
 
             // Should we ignore the resistance altogether?
             if (passCfg->always ||
-                (newMonitor == brokeFromMonitor && 
-                (current.moveTimepoint - brokeFromTimepoint) < passCfg->returnBefore))
+                (newMonitor == brokeFromMonitor &&
+                 (current.moveTimepoint - brokeFromTimepoint) < passCfg->returnBefore))
             {
                 pass = true;
             }
@@ -338,7 +420,7 @@ void pointerMoved(Time time, int x, int y, double dx, double dy)
                         resistanceFactor = std::pow(resistanceFactor, cfgResistanceSpeedupExponent);
 
                     resistanceFactor *= std::pow(std::abs(speed1 - speed2) / std::max(speed1, speed2), cfgResistanceConstSpeedExponent);
-                
+
                     if (onVerEdge && dx != 0.0)
                         resistanceFactor *= std::pow(speed2 / std::abs(dx), cfgResistanceDirectionExponent);
                     else if (onHorEdge && dy != 0.0)
@@ -378,34 +460,28 @@ void pointerMoved(Time time, int x, int y, double dx, double dy)
             }
             else
             {
-                if (x <= currentMonitor->x + cfgResistanceMargins && dx <= 0)
-                    x = currentMonitor->x + cfgResistanceMargins;
-                if (y <= currentMonitor->y + cfgResistanceMargins && dy <= 0)
-                    y = currentMonitor->y + cfgResistanceMargins;
-                if (x > currentMonitor->x + currentMonitor->w - cfgResistanceMargins && dx >= 0)
-                    x = currentMonitor->x + currentMonitor->w - cfgResistanceMargins - 1;
-                if (y > currentMonitor->y + currentMonitor->h && dy >= 0)
-                    y = currentMonitor->y + currentMonitor->h - cfgResistanceMargins - 1;
-
                 /*
-                Manually setting the position causes the pointer to 'flicker' because of the delay
-                between the movePointer() call and actual pointer update on screen.
-                We confine the pointer in the previous window for cases when there is a fullscreen window.
+                Manually setting the position causes the pointer to 'flicker'
+                because of the delay between the movePointer() call and actual
+                pointer update on screen. We confine the pointer in window
+                spanning the whole monitor.
                 */
-                movePointer(x, y);
+                confinePointer(currentMonitor);
             }
-            
         }
-        else if (currentMonitor->contains(x, y, cfgResistanceMargins+1))
+        else
         {
-            onEdge = false;
+            unconfinePointer();
+            if (currentMonitor->contains(x, y, cfgResistanceMargins + 1))
+            {
+                onEdge = false;
+            }
         }
     }
     else
     {
         currentMonitor = getMonitorAt(x, y);
     }
-    
 }
 
 /*
